9f8e57ed00e3f32cc3f025c92c7cc76985ae6ba7
PrinceNarteh <prinarteh@gmail.com>
2021-09-24 06:30:22 +0000

    Styled Home Screen

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -14,45 +14,69 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     data = ModalRoute.of(context)!.settings.arguments as Map;
 
+    // set background
+    late String bgImage = data['isDayTime'] ? 'day.png' : 'night.png';
+    late Color bgColor =
+        data['isDayTime'] ? Colors.blue : Colors.indigo[700] as Color;
+
     return Scaffold(
+      backgroundColor: bgColor,
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.fromLTRB(0, 120, 0, 0),
-          child: Column(
-            children: [
-              TextButton.icon(
-                onPressed: () {
-                  Navigator.pushNamed(context, "location");
-                },
-                icon: const Icon(Icons.edit_location),
-                label: const Text("Edit Location"),
-              ),
-              const SizedBox(
-                height: 20.0,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    data['location'],
-                    style: const TextStyle(
-                      fontSize: 30,
-                      letterSpacing: 2.0,
+        child: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/$bgImage'),
+              fit: BoxFit.cover,
+            ),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(0, 120, 0, 0),
+            child: Column(
+              children: [
+                TextButton.icon(
+                  onPressed: () {
+                    Navigator.pushNamed(context, "location");
+                  },
+                  icon: const Icon(
+                    Icons.edit_location,
+                    color: Colors.grey,
+                  ),
+                  label: const Text(
+                    "Edit Location",
+                    style: TextStyle(
+                      color: Colors.grey,
                     ),
-                  )
-                ],
-              ),
-              const SizedBox(
-                height: 20.0,
-              ),
-              Text(
-                data['time'],
-                style: const TextStyle(
-                  fontSize: 65,
-                  letterSpacing: 2.0,
+                  ),
                 ),
-              )
-            ],
+                const SizedBox(
+                  height: 20.0,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      data['location'],
+                      style: const TextStyle(
+                        fontSize: 30,
+                        letterSpacing: 2.0,
+                        color: Colors.white,
+                      ),
+                    )
+                  ],
+                ),
+                const SizedBox(
+                  height: 20.0,
+                ),
+                Text(
+                  data['time'],
+                  style: const TextStyle(
+                    fontSize: 65,
+                    letterSpacing: 2.0,
+                    color: Colors.white,
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
